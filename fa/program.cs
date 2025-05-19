d1e50dd29cda2248f6aaa36f763c89bda93b32ca
DioHolyDiver <163848776+DioHolyDiver@users.noreply.github.com>
2025-05-20 00:27:48 +0300

    B99 lab2 check

--- fa/program.cs
+++ fa/program.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace mod_lab02_fa_csharp.fa
+namespace fans
 {
     public class State
     {
-        public string Name { get; private set; }
-        public Dictionary<char, State> Transitions = new();
-        public bool IsAcceptState { get; private set; }
+        public string Name { get; }
+        public Dictionary<char, State> Transitions { get; } = new Dictionary<char, State>();
+        public bool IsAcceptState { get; }
 
         public State(string name, bool isAcceptState)
         {
@@ -18,107 +17,51 @@ namespace mod_lab02_fa_csharp.fa
             IsAcceptState = isAcceptState;
         }
     }
-
-
-    public abstract class Fa
+    public class FiniteAutomaton
     {
-        protected bool Run(IEnumerable<char> s, State initState)
-        {
-            var current = initState;
-            foreach (var c in s)
-            {
-                if (current is null || !current.Transitions.TryGetValue(c, out current))
-                    return false;
-            }
+        private readonly State initialState;
 
+        public FiniteAutomaton()
+        {
+            var stateA = new State("a", false);
+            var stateB = new State("b", false);
+            var stateC = new State("c", true);
+            stateA.Transitions.Add('0', stateA);
+            stateA.Transitions.Add('1', stateB);
+            stateB.Transitions.Add('0', stateC);
+            stateB.Transitions.Add('1', stateA);
+            stateC.Transitions.Add('0', stateB);
+            stateC.Transitions.Add('1', stateC);
+            initialState = stateA;
+        }
+        public bool? Run(IEnumerable<char> symbols)
+        {
+            State current = initialState;
+            foreach (var symbol in symbols)
+            {
+                if (!current.Transitions.ContainsKey(symbol))
+                    return null;
+                current = current.Transitions[symbol];
+            }
             return current.IsAcceptState;
         }
     }
-
-    public class Fa1 : Fa
-    {
-        private readonly State _initState;
-        public Fa1()
-        {
-            var a = new State("a", false);
-            var b = new State("b", false);
-            var c = new State("c", false);
-            var d = new State("d", true);
-            a.Transitions['0'] = c;
-            a.Transitions['1'] = b;
-            b.Transitions['1'] = a;
-            b.Transitions['0'] = d;
-            c.Transitions['1'] = d;
-            d.Transitions['1'] = d;
-
-            _initState = a;
-        }
-
-        public bool Run(IEnumerable<char> s) => Run(s, _initState);
-    }
-
-    public class Fa2 : Fa
-    {
-        private readonly State _initState;
-
-        public Fa2()
-        {
-            var a = new State("a", false);
-            var b = new State("b", false);
-            var c = new State("c", false);
-            var d = new State("d", true);
-            a.Transitions['0'] = c;
-            a.Transitions['1'] = b;
-            c.Transitions['0'] = a;
-            c.Transitions['1'] = d;
-            b.Transitions['0'] = d;
-            b.Transitions['1'] = a;
-            d.Transitions['0'] = b;
-            d.Transitions['1'] = c;
-
-            _initState = a;
-        }
-
-        public bool Run(IEnumerable<char> s) => Run(s, _initState);
-    }
-
-    public class Fa3 : Fa
-    {
-        private readonly State _initState;
-
-        public Fa3()
-        {
-            var a = new State("a", false);
-            var b = new State("b", false);
-            var c = new State("c", true);
-
-            a.Transitions['0'] = a;
-            a.Transitions['1'] = b;
-            b.Transitions['0'] = a;
-            b.Transitions['1'] = c;
-            c.Transitions['0'] = c;
-            c.Transitions['1'] = c;
-
-            _initState = a;
-        }
-
-        public bool Run(IEnumerable<char> s) => Run(s, _initState);
-    }
-
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            const string str = "01111";
-            var fa1 = new Fa1();
-            var fa2 = new Fa2();
-            var fa3 = new Fa3();
-            var result1 = fa1.Run(str);
-            var result2 = fa2.Run(str);
-            var result3 = fa3.Run(str);
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
-            Console.WriteLine(result3);
+            string input = "0000010111";
+            FiniteAutomaton automaton = new FiniteAutomaton();
+            bool? result = automaton.Run(input);
+
+            if (result.HasValue)
+            {
+                Console.WriteLine($"Последовательность принята: {result.Value}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный символ в последовательности");
+            }
         }
     }
 }
