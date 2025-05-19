b83c2dd6e0f71f0ddc71e7a4fd803fdf46c38ab4
DioHolyDiver <163848776+DioHolyDiver@users.noreply.github.com>
2025-05-20 02:02:11 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -10,19 +10,23 @@ namespace fans
         public string Name { get; }
         public Dictionary<char, State> Transitions { get; } = new Dictionary<char, State>();
         public bool IsFinal { get; }
+
         public State(string name, bool isFinal)
         {
             Name = name;
             IsFinal = isFinal;
         }
     }
+
     public abstract class FiniteAutomata
     {
         protected readonly State StartState;
+
         protected FiniteAutomata(State startState)
         {
             StartState = startState;
         }
+
         public bool Run(string input)
         {
             State current = StartState;
@@ -35,62 +39,80 @@ namespace fans
             return current.IsFinal;
         }
     }
+
     public class FA1 : FiniteAutomata
     {
         public FA1() : base(CreateStates()) { }
+
         private static State CreateStates()
         {
             var q0 = new State("q0", false);
             var q1 = new State("q1", false);
             var q2 = new State("q2", true);
+
             q0.Transitions['0'] = q0;
             q0.Transitions['1'] = q1;
+
             q1.Transitions['0'] = q1;
             q1.Transitions['1'] = q2;
+
             q2.Transitions['0'] = q2;
             q2.Transitions['1'] = q0;
+
             return q0;
         }
     }
-}
+
     public class FA2 : FiniteAutomata
     {
         public FA2() : base(CreateStates()) { }
+
         private static State CreateStates()
         {
             var a = new State("a", false);
             var b = new State("b", false);
             var c = new State("c", false);
             var d = new State("d", true);
+
             a.Transitions['0'] = b;
             a.Transitions['1'] = c;
+
             b.Transitions['0'] = a;
             b.Transitions['1'] = d;
+
             c.Transitions['0'] = d;
             c.Transitions['1'] = a;
+
             d.Transitions['0'] = c;
             d.Transitions['1'] = b;
+
             return a;
         }
     }
+
     public class FA3 : FiniteAutomata
     {
         public FA3() : base(CreateStates()) { }
+
         private static State CreateStates()
         {
             var a = new State("a", false);
             var b = new State("b", false);
             var c = new State("c", true);
+
             a.Transitions['0'] = a;
             a.Transitions['1'] = b;
+
             b.Transitions['0'] = a;
             b.Transitions['1'] = c;
+
             c.Transitions['0'] = c;
             c.Transitions['1'] = c;
 
             return a;
         }
     }
+
     class Program
     {
         static void Main(string[] args)
